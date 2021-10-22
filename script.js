a3bb1f3de0b30d5d747c7c20c5801ef12bfdf79f
unthinkmedia <57981398+unthinkmedia@users.noreply.github.com>
2021-10-22 10:49:37 -0400

    done

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 //Example 1: Go to another URL or file
 let goto_btn = document.querySelector("#goto_btn");
 
+goto_btn.addEventListener("click", event => {
+    document.location.href = "http://www.google.com";
+})
+
 
 //Example 2: Pretend we are checking for passwords
 
@@ -18,12 +22,41 @@ let pretendDatabase = {
     }
 }
 
+signin_btn.addEventListener("click", event => {
+    let username = uname_txt.value;
+    let password = password_txt.value;
+
+    if(pretendDatabase[username]){
+        console.log("PASSED")
+        if(password == pretendDatabase[username].password){
+            console.log("Signed IN!");
+        }else{
+            console.log("Bad password");
+        }
+
+    }else{
+        console.log("This user does not exist");
+    }
+});
+
+
+
 //Example 3: Do a word count
 
 let message_txt = document.querySelector("#message_txt");
 let notification_txt = document.querySelector("#notification_txt")
 let wordCount = document.querySelector("#wordCount");
 
+message_txt.addEventListener("input", event => {
+    wordCount.innerText = message_txt.value.length;
+
+    if(message_txt.value.length > 30){
+        notification_txt.classList.add("warning");
+    }else{
+        notification_txt.classList.remove("warning");
+    }
+
+})
 
 
 //Example 4: Us values somewhere else
@@ -33,4 +66,7 @@ let range_output = document.querySelector('#range_output');
 range_output.innerHTML = range_input.value;
 scaleMe_ele.style.width = range_input.value;
 
-
+range_input.addEventListener("input", event => {
+    range_output.innerHTML = range_input.value;
+    scaleMe_ele.style.width = range_input.value + "px";
+})
